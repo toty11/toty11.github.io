e91420c62b0db7013bfaa3a3329fc039734e65e4
toty11 <xtoty971@gmail.com>
2018-07-02 11:38:45 -0400

    test1 sysinv

---
+++ main.js
@@ -0,0 +1,19 @@
+function smoothScrollingTo(target){
+    $('html,body').animate({scrollTop:$(target).offset().top}, 500);
+}
+
+  $('a[href*=\\#]').on('click', function(event){     
+      event.preventDefault();
+      smoothScrollingTo(this.hash);
+  });
+
+  
+  $(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#arrow').fadeIn();
+        } else {
+            $('#arrow').fadeOut();
+        }
+    });
+  });
